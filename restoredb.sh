9d9382d047c097c43a18a221555b88ea2ca975bb
aagent297 <49935683+Tallguy297@users.noreply.github.com>
2023-01-14 20:48:52 +1100

    Update restoredb.sh

--- restoredb.sh
+++ restoredb.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env bash
 
 ## Restore KODI Database...
-rm -f -R 0777 /home/kodi/
+rm -f -r /home/kodi/
 cp -v -f -r ../kodicfg/. /home/
 chmod -f -R 0777 /home/kodi/
 
 ## Restore USERS Database...
-rm -f -R 0777 /home/$(users | awk '{print $1}')/
+rm -f -r /home/$(users | awk '{print $1}')/
 cp -v -f -r ../homecfg/. /home/
+rm -f -r /home/$(users | awk '{print $1}')/.config/pulse
 chmod -f -R 0777 /home/$(users | awk '{print $1}')/
