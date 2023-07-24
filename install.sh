d371ed963e9dc8ea89c00bed10ed9aaaa518659a
aagent297 <49935683+Tallguy297@users.noreply.github.com>
2023-07-24 10:32:49 +1000

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ currentuser=$(users | awk '{print $1}')
 
 function run-in-user-session() {
     _display_id=":$(find /tmp/.X11-unix/* | sed 's#/tmp/.X11-unix/X##' | head -n 1)"
-    _username=$(who | grep "\(${_display_id}\)" | awk '{print $1}')
+    _username=$(who | grep "($_display_id)" | awk '{print $1}')
     _user_id=$(id -u "$_username")
     _environment=("DISPLAY=$_display_id" "DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/$_user_id/bus")
     sudo -Hu "$_username" env "${_environment[@]}" "$@"
